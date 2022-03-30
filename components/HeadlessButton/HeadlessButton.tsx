98759c346912191d3a3c6e02fdd1966b49222404
Alexey Ivanov <mail@alexeyivanov.info>
2022-03-31 01:46:31 +0900

    Add forwrdRef to HeadlessButton

--- components/HeadlessButton/HeadlessButton.tsx
+++ components/HeadlessButton/HeadlessButton.tsx
@@ -1,15 +1,24 @@
+import { forwardRef, RefObject } from "react";
 import cn from "classnames";
 import { DetailedHTMLProps, ButtonHTMLAttributes } from "react";
 import styles from "./HeadlessButton.module.css";
 
-export const HeadlessButton = ({
-  className,
-  ...props
-}: DetailedHTMLProps<
-  ButtonHTMLAttributes<HTMLButtonElement>,
-  HTMLButtonElement
->) => {
-  return <button {...props} className={cn(styles.wrapper, className)} />;
-};
+// eslint-disable-next-line react/display-name
+export const HeadlessButton = forwardRef(
+  (
+    {
+      className,
+      ...props
+    }: DetailedHTMLProps<
+      ButtonHTMLAttributes<HTMLButtonElement>,
+      HTMLButtonElement
+    >,
+    ref: RefObject<HTMLButtonElement>
+  ) => {
+    return (
+      <button {...props} className={cn(styles.wrapper, className)} ref={ref} />
+    );
+  }
+);
 
 export default HeadlessButton;
