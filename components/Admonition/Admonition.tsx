045a795baea3d3ceed7cb10f49f7096213e183b7
Katrin Shibakova <katrina.gubanova@gmail.com>
2022-04-07 12:54:30 +0100

    added a scope condition for the Admonition component

--- components/Admonition/Admonition.tsx
+++ components/Admonition/Admonition.tsx
@@ -1,4 +1,7 @@
 import cn from "classnames";
+import { useContext, useMemo } from "react";
+import { DocsContext, getScopes } from "layouts/DocsPage/context";
+import { ScopesType } from "layouts/DocsPage/types";
 import styles from "./Admonition.module.css";
 
 const capitalize = (text: string): string =>
@@ -10,13 +13,31 @@ export interface AdmonitionProps {
   type: typeof types[number];
   title: string;
   children: React.ReactNode;
+  scopeOnly: boolean;
+  scope?: ScopesType;
 }
 
-const Admonition = ({ type = "tip", title, children }: AdmonitionProps) => {
+const Admonition = ({
+  type = "tip",
+  title,
+  children,
+  scopeOnly = false,
+  scope,
+}: AdmonitionProps) => {
   const resolvedType = type && types.includes(type) ? type : "tip";
+  const { scope: currentScope } = useContext(DocsContext);
+  const scopes = useMemo(() => getScopes(scope), [scope]);
+  const isInCurrentScope = scopes.includes(currentScope);
+  const isHidden = scopeOnly && !isInCurrentScope;
 
   return (
-    <div className={cn(styles.wrapper, styles[resolvedType])}>
+    <div
+      className={cn(
+        styles.wrapper,
+        isHidden && styles.hidden,
+        styles[resolvedType]
+      )}
+    >
       <div className={styles.header}>{title || capitalize(type)}</div>
       <div className={styles.body}>{children}</div>
     </div>
