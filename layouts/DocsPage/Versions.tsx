40a07fcffe54ba87cc82c81beffc1f0c78505300
Katrin Shibakova <katrina.gubanova@gmail.com>
2022-04-12 13:26:21 +0300

    versioning is configured without moving to the start page

--- layouts/DocsPage/Versions.tsx
+++ layouts/DocsPage/Versions.tsx
@@ -2,6 +2,7 @@ import cn from "classnames";
 import { useRouter } from "next/router";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { Dropdown } from "components/Dropdown";
+import { splitPath, buildPath } from "utils/url";
 import type { VersionsInfo } from "./types";
 import styles from "./Versions.module.css";
 
@@ -16,18 +17,27 @@ const Versions = ({
 }: VersionsInfo) => {
   const router = useRouter();
   const [currentItem, setCurrentItem] = useState<string>(current);
+  const path = splitPath(router.asPath).path.split("/");
+  console.log(path);
+  let currentPage = "";
+  if (path.includes("ver")) {
+    currentPage = path.splice(3).join("/");
+  } else {
+    currentPage = path.join("/");
+  }
+  console.log(currentPage);
 
   const versions = useMemo(() => [...available].reverse(), [available]);
-
+  console.log(latest);
   const navigateToVersion = useCallback(
     (version: string) => {
       const isLatest = version === latest;
-      const href = `${isLatest ? "/" : `/ver/${version}`}`;
+      const href = `${isLatest ? "/" : `/ver/${version}/${currentPage}`}`;
 
       setCurrentItem(version);
       router.push(href);
     },
-    [latest, router]
+    [latest, router, currentPage]
   );
 
   useEffect(() => {
