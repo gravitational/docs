9751fb8179d3aee33eb9812e35c90dfe026d764d
Katrin Shibakova <katrina.gubanova@gmail.com>
2022-04-25 18:49:02 +0300

    added the bash lang in the remark-code-snippet

--- server/mdx-config-docs.ts
+++ server/mdx-config-docs.ts
@@ -88,7 +88,7 @@ const config: MdxConfig = {
         },
       },
     ],
-    remarkCodeSnippet, // Plugin for custom code snippets with multiple copy buttons
+    [remarkCodeSnippet, { langs: ["code", "bash"] }], // Plugin for custom code snippets with multiple copy buttons
     remarkGFM, // Adds tables
     remarkImportFiles, // Replaces paths to files with imports
     remarkLinks, // Make links in docs absolute with /ver/X.X included
