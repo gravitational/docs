e1f682e5af17af00708cc84d9706e11a1a830876
Alexey Ivanov <mail@alexeyivanov.info>
2022-05-21 01:08:23 +0900

    Cleanup and comments

--- server/docs-helpers.ts
+++ server/docs-helpers.ts
@@ -2,12 +2,11 @@
  * Bunch of helper functions to help with docs generation.
  */
 
-import type { VFile } from "vfile";
-
 import { resolve } from "path";
 import { getPathWithoutVersion } from "../utils/url";
 import { loadConfig as loadDocsConfig } from "./config-docs";
 import { loadConfig as loadSiteConfig } from "./config-site";
+
 import type {
   RawNavigationItem,
   NavigationItem,
@@ -15,6 +14,12 @@ import type {
   ScopesInMeta,
   ComplexScopesConfig,
 } from "layouts/DocsPage/types";
+
+import { getStaticPathsForDocs, getDocsPagesMap } from "./paths";
+import { getPageInfo } from "./pages-helpers";
+import { fetchVideoMeta } from "./youtube-meta";
+import getHeaders from "./get-headers";
+import { transformToAST } from "./markdown-config";
 
 const { branches, versions, latest } = loadSiteConfig();
 const NEXT_PUBLIC_GITHUB_DOCS = process.env.NEXT_PUBLIC_GITHUB_DOCS;
@@ -170,5 +175,68 @@ export const getPageMeta = (path: string) => {
       available: versions,
     },
     scopes,
+  };
+};
+
+/*
+ * Generate and return props for each docs page. For production it will use
+ * cached results, for dev mode it will regenerate them each time.
+ */
+
+const cachedGetStaticPathsForDocs = getStaticPathsForDocs();
+
+export const getDocsPaths = () => {
+  if (process.env.NODE_ENV === "production") {
+    return cachedGetStaticPathsForDocs;
+  } else {
+    return getStaticPathsForDocs();
+  }
+};
+
+/*
+ * Generates and return a map of the slugs and filenames. For production it will use
+ * cached results, for dev mode it will regenerate them each time.
+ */
+
+const cachedPagesMap = getDocsPagesMap();
+
+const getPagesMap = () => {
+  if (process.env.NODE_ENV === "production") {
+    return cachedPagesMap;
+  } else {
+    return getDocsPagesMap();
+  }
+};
+
+/*
+ * Returns props for the docs page.
+ */
+
+export const getDocsPageProps = async (slug: string) => {
+  const filepath = getPagesMap()[slug];
+
+  // Read vfile with parsed frontmatter
+  const page = getPageInfo(filepath);
+
+  // Adds versions, navigation, githubUrl, etc.
+  const pageMeta = getPageMeta(filepath);
+
+  // If we have videoBanner file in config, we load this vide data through YouTube API.
+  const { videoBanner } = page.data.frontmatter;
+
+  if (typeof videoBanner === "string") {
+    page.data.frontmatter.videoBanner = await fetchVideoMeta(videoBanner);
+  }
+
+  // Transforms page text page to ASK while applying bunch of plugins
+  const AST = await transformToAST(page.data.content, page);
+
+  // Generates ToC from the headers in the AST
+  const tableOfConents = getHeaders(AST);
+
+  return {
+    meta: { ...page.data.frontmatter, ...pageMeta },
+    AST,
+    tableOfConents,
   };
 };
--- server/rehype-mdx-to-hast.ts
+++ server/rehype-mdx-to-hast.ts
@@ -1,6 +1,5 @@
 /*
- * this is a plugin that converts suset of mdx nodes to hast.
- *
+ * this is a plugin that converts subset of mdx nodes to hast.
  */
 
 import type { Transformer } from "unified";
@@ -19,17 +18,18 @@ import type {
 
 import { visit } from "unist-util-visit";
 
+// All the types of node that can be inside MDX file
 type MdxNode =
-  | MdxJsxFlowElement
-  | MdxJsxTextElement
-  | MDXJSEsm
-  | MDXFlowExpression
-  | MDXTextExpression;
+  | MdxJsxFlowElement // https://github.com/syntax-tree/mdast-util-mdx-jsx/blob/main/lib/complex-types.d.ts
+  | MdxJsxTextElement // https://github.com/syntax-tree/mdast-util-mdx-jsx/blob/main/lib/complex-types.d.ts
+  | MDXJSEsm // https://github.com/syntax-tree/mdast-util-mdxjs-esm/blob/main/complex-types.d.ts
+  | MDXFlowExpression // https://github.com/syntax-tree/mdast-util-mdx-expression/blob/main/complex-types.d.ts
+  | MDXTextExpression; // https://github.com/syntax-tree/mdast-util-mdx-expression/blob/main/complex-types.d.ts
 
 export default function rehypeMdxToHast(): Transformer {
   return (root: Parent) => {
     visit(root, (node: MdxNode, index: number, parent: Parent) => {
-      // https://github.com/syntax-tree/mdast-util-mdx-jsx/blob/main/lib/complex-types.d.ts
+      // TextElement in an inline tag and FlowElement is a block tag. e want ot convert them.
       if (
         node.type === "mdxJsxTextElement" ||
         node.type === "mdxJsxFlowElement"
@@ -39,12 +39,18 @@ export default function rehypeMdxToHast(): Transformer {
           tagName: node.name.toLowerCase(),
           properties: (node.attributes as MdxJsxAttribute[]).reduce(
             (result, prop) => {
+              // If the prop in markdown was a js-expression like disabled={true}
+              // it will be parsed as an object with original value as a string
+              // and as a estree. For now we just use the string value.
               if (typeof prop.value === "object" && prop.value !== null) {
+                // Temporary fix for scopeOnly={true}, can be removed after docs update
                 if (prop.name === "scopeOnly") {
                   return {
                     ...result,
                     [prop.name]: null,
                   };
+                  // Temporary fix for scope={["oss", "cloud"]}, will transform value
+                  // to scope="oss,cloud". Can be removed after docs update.
                 } else if (prop.name === "scope") {
                   return {
                     ...result,
@@ -57,6 +63,8 @@ export default function rehypeMdxToHast(): Transformer {
                   };
                 }
 
+                // By default just return value as is. E. g. field={something}
+                // will get value "{something}"" as is with curly braces.
                 return {
                   ...result,
                   [prop.name]: (prop.value as MdxJsxAttributeValueExpression)
@@ -64,6 +72,7 @@ export default function rehypeMdxToHast(): Transformer {
                 };
               }
 
+              // for non-js nodes just return value
               return { ...result, [prop.name]: prop.value };
             },
             {}
@@ -72,9 +81,9 @@ export default function rehypeMdxToHast(): Transformer {
         };
 
         parent.children[index] = newNode;
+        // This is a pure JS nodes, like {1 + 1} or imports/exports.
+        // Just removing them for now for simplicity.
       } else if (
-        // https://github.com/syntax-tree/mdast-util-mdx-expression/blob/main/complex-types.d.ts
-        // https://github.com/syntax-tree/mdast-util-mdxjs-esm/blob/main/complex-types.d.ts
         ["mdxFlowExpression", "mdxTextExpression", "mdxjsEsm"].includes(
           node.type
         )
