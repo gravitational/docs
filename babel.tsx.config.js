62160042bec4aaed49dd314f4cb4ed9559467f26
Paul Gottschling <paul.gottschling@goteleport.com>
2022-10-04 12:25:32 -0400

    Add a broken postcss config to the babel config

--- babel.tsx.config.js
+++ babel.tsx.config.js
@@ -1,12 +1,13 @@
 // Babel config used by Jest tests for React components
 // See: https://jestjs.io/docs/tutorial-react
 module.exports = {
-  presets: [
-    "@babel/preset-env",
-    ["@babel/preset-typescript", { 
-    	isTSX: true,
-    	allExtensions: true
-    }],
-    ["@babel/preset-react", { runtime: "automatic" }],
+  plugins: [
+    [
+      "babel-plugin-postcss",
+      {
+        postcss: true,
+      },
+    ],
   ],
+  presets: ["next/babel"],
 };
