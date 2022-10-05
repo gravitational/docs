f60422eef5a02619ded6e1b8c8e59fb42a613728
Paul Gottschling <paul.gottschling@goteleport.com>
2022-10-05 14:34:46 -0400

    Transform SVGs into React components

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,32 @@
 // Following the example here:
 // https://nextjs.org/docs/testing#setting-up-jest-with-the-rust-compiler
-const nextJest = require('next/jest')
+const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
-})
+  dir: "./",
+});
 
 const customJestConfig = {
-  moduleDirectories: ['node_modules', '<rootDir>/'],
-  testEnvironment: 'jest-environment-jsdom',
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  testEnvironment: "jest-environment-jsdom",
   // Ignore resource queries in Webpack module rules. For context:
   // https://webpack.js.org/configuration/module/#ruleresourcequery
   moduleNameMapper: {
-      '(^.*\\.\\w{3})\\?\\w+$': '$1'
-  }
-}
+    "(^.*\\.\\w{3})\\?\\w+$": "$1",
+  },
+  transform: {
+    "\\.svg$": [
+      "babel-jest",
+      {
+        presets: [
+          "@svgr/babel-preset",
+          "next/babel"
+        ]
+      },
+    ],
+  },
+};
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig)
+module.exports = createJestConfig(customJestConfig);
