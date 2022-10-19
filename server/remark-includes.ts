b1ff5ba058ac658d79606c370e3f33cb05638d03
Paul Gottschling <paul.gottschling@goteleport.com>
2022-10-19 13:00:15 -0400

    Add support for basic named params in partials
    Closes #165
    
    Allow a partial inclusion expression, e.g., `(!mypartial.mdx!)` to
    assign named parameters that replace template variables in partials. For
    example, `(!mypartial.mdx var="value"!)` replaces the template variable
    `{{ var }}` in `mypartial.mdx` with `value`.
    
    Design details:
    
    - Variable values must be in quotes, single or double, and the quotes
      must match.
    
    - There can be multiple variables/values. The inclusion expression
      separates these with spaces, e.g.:
    
      ```
      (!mypartial.mdx var1="val" var2='val'!)
      ```
    
    - Allows for default parameter values, which will make it easier to add
      parameter support to existing partials. E.g., with a template variable
      like this:
    
      ```
      {{ myvar="myval" }}
      ```
    
      This partial will populate the template variable with `myval` at this
      particular location if the inclusion expression does not assign it a
      value.
    
    - If a parameter does _not_ have a default value, and an inclusion
      expression does not provide one, the `{{ myvar }}` expression will be
      left untouched, allowing for situations where a partial needs to
      include a literal `{{ }}` string.
    
    - Escapes: Unfortunately, due to some JavaScript quirks I didn't give
      myself time to look into, quotes must be escaped twice, e.g., `\\"`.
      Double escapes are removed within double-quoted strings but not within
      single-quoted strings.

--- server/remark-includes.ts
+++ server/remark-includes.ts
@@ -28,15 +28,135 @@ import { frontmatterFromMarkdown } from "mdast-util-frontmatter";
 
 import updateMessages from "./update-vfile-messages";
 
-const includeRegexpBase = "\\(!([^!]+)!\\)`?";
+const includeRegexpBase = "\\(!(.*)!\\)`?";
 const includeRegexp = new RegExp(includeRegexpBase);
 const exactIncludeRegexp = new RegExp(`^${includeRegexpBase}$`);
 const globalIncludeRegexp = new RegExp(includeRegexpBase, "g");
+const varDefaultValueRegexp = new RegExp(`{{ ?.+=["']([^"']+)["'] ?}}`, "g");
 
 interface ResolveIncludesProps {
   value: string;
   filePath: string;
   rootDir: string;
+}
+
+export interface ParameterAssignments {
+  [index: string]: string;
+}
+
+// parsePartialParams takes a partial inclusion expression and parses the
+// parameter assignments that follow the filepath. For example, if the
+// expression is:
+//
+// (!partial.mdx var1="val" var2="val"!)
+//
+// ...it returns a ParameterAssignments with keys for "var1" and "var2".
+// This function is intended to separate different parameter assignments for
+// processing downstream.
+//
+// TODO(ptgott): I wasn't able to use regular expressions for this, and the
+// current approach is pretty ugly. There's got to be a cleaner, more efficient
+// way.
+export function parsePartialParams(expr: string): ParameterAssignments {
+  if (!expr) {
+    return {};
+  }
+
+  if (!exactIncludeRegexp.test(expr)) {
+    throw new Error("the expression does not include a partial");
+  }
+
+  // extract everything after the filepath and before the "!)"
+  const val = expr.slice(expr.indexOf(" "), expr.length - 2);
+
+  const keyRegexp = new RegExp(`[^= "']`);
+  let assignments: ParameterAssignments = {};
+  let inQuotedString: boolean = false;
+  let currentQuoteStyle: string = "";
+  let inKey: boolean = false;
+  let currentKey: string = "";
+  let currentVal: string = "";
+  for (let c = 0; c < val.length; c++) {
+    // We find a quote that's not escaped
+    if ((val[c] == `"` || val[c] == "'") && (c == 0 || val[c - 1] !== "\\")) {
+      inKey = false;
+      // This is the beginning of a quoted string
+      if (!inQuotedString) {
+        inQuotedString = true;
+        currentQuoteStyle = val[c];
+        currentVal += val[c];
+        continue;
+        // We have reached the end of a parameter value, so store it in the map
+        // map of parameter assignments along with its corresponding parameter
+        // name.
+        // If there's an unescaped quote of a different style than the current
+        // quote, ignore it for now.
+      } else if (currentQuoteStyle == val[c]) {
+        currentVal += val[c];
+        currentQuoteStyle = "";
+        inQuotedString = false;
+        assignments[currentKey] = currentVal;
+        currentKey = "";
+        currentVal = "";
+        continue;
+      }
+    }
+
+    // We're in a quoted string but the quotation hasn't been terminated
+    if (!!inQuotedString) {
+      currentVal += val[c];
+    }
+
+    // We find the start of a parameter name
+    if (keyRegexp.test(val[c]) && !inQuotedString && !inKey) {
+      inKey = true;
+      currentKey += val[c];
+      continue;
+    }
+
+    // We find part of a parameter name
+    if (!!inKey && keyRegexp.test(val[c])) {
+      currentKey += val[c];
+      continue;
+    }
+
+    // We find something that's not a quote or a parameter name, e.g., a space
+    // or "!" near the end of input.
+    if (!!inKey && !keyRegexp.test(val[c])) {
+      inKey = false;
+      continue;
+    }
+  }
+  return assignments;
+}
+
+// resolveParamValue takes the quoted value of a template variable within a
+// partial and returns the unquoted value, resolving any escapes. Escaped
+// quotations are resolved within double-quoted strings but not single-quoted
+// strings.
+//
+// Throws an exception if the quotes are mismatched, the input is not wrapped
+// in quotes, or the input is empty
+export function resolveParamValue(val: string): string {
+  if (val == "") {
+    throw new Error("the value of a parameter in a partial cannot be empty");
+  }
+  if (val[0] !== `"` && val[0] !== `'`) {
+    throw new Error(
+      `parameter values in partials must be wrapped in strings, but this value is not: ${val}`
+    );
+  }
+  if (val[0] !== val[val.length - 1]) {
+    throw new Error(
+      `parameter values in partials must be wrapped in matching quotes, but this value is not: ${val}`
+    );
+  }
+  let newVal = val.slice(1, val.length - 1);
+  if (val[0] == '"') {
+    newVal = newVal.replaceAll('\\"', '"');
+  }
+
+  return newVal;
 }
 
 const resolveIncludes = ({
@@ -46,11 +166,40 @@ const resolveIncludes = ({
 }: ResolveIncludesProps) => {
   let error: string;
 
+  // Assemble an object where keys are the variable keys and values are the var
+  // values.
+  let vars = {};
+
   const result = value.replace(includeRegexp, (_, includePath) => {
+    includePath = includePath.split(" ")[0];
     const fullImportPath = join(rootDir, includePath);
 
     if (existsSync(fullImportPath)) {
-      return readFileSync(fullImportPath, "utf-8");
+      let content = readFileSync(fullImportPath, "utf-8");
+
+      const inclusions = value.matchAll(globalIncludeRegexp);
+      for (const inclusion of Array.from(inclusions)) {
+        const varAssignments = parsePartialParams(inclusion[0]);
+
+        // Replace variables in the partial with their values
+        Object.keys(varAssignments).forEach((param) => {
+          const val = resolveParamValue(varAssignments[param]);
+          const varRegexp = new RegExp(
+            `{{ ?${param}(=["'][^"']+["'])? ?}}`,
+            "g"
+          );
+          content = content.replaceAll(varRegexp, val);
+        });
+
+        // At this point, we have replaced template variables that were supplied
+        // values, so the remainder either have default values or were
+        // incorrectly given no value.
+        content = content.replace(varDefaultValueRegexp, (_, val) => {
+          return val;
+        });
+      }
+
+      return content;
     } else {
       error = `Wrong import path ${includePath} in file ${filePath}.`;
 
@@ -158,7 +307,7 @@ export default function remarkIncludes({
               const path = node.value.match(exactIncludeRegexp)[1];
 
               if (resolve) {
-                if (path.match(/\.mdx?$/)) {
+                if (path.split(" ")[0].match(/\.mdx?$/)) {
                   const tree = fromMarkdown(result, {
                     extensions: [mdxjs(), gfm(), frontmatter()],
                     mdastExtensions: [
--- uvu-tests/remark-includes.test.ts
+++ uvu-tests/remark-includes.test.ts
@@ -8,7 +8,10 @@ import { remark } from "remark";
 import remarkMdx from "remark-mdx";
 import remarkGFM from "remark-gfm";
 import remarkIncludes, {
+  ParameterAssignments,
   RemarkIncludesOptions,
+  parsePartialParams,
+  resolveParamValue,
 } from "../server/remark-includes";
 
 const transformer = (
@@ -104,6 +107,209 @@ Suite("Multiple includes resolve in code block", () => {
     resolve("server/fixtures/includes-multiple-result.mdx"),
     "utf-8"
   );
+
+  assert.equal(result, expected);
+});
+
+Suite("resolveParamValue handles quotes correctly in parameter values", () => {
+  interface testCase {
+    description: string;
+    input: string;
+    shouldThrow: boolean;
+    expected?: string;
+  }
+  const cases: testCase[] = [
+    {
+      description: "mismatched quotes",
+      input: `"This is a string'`,
+      shouldThrow: true,
+    },
+    {
+      description: "double quotes containing single quotes",
+      input: `"This is a 'string'"`,
+      shouldThrow: false,
+      expected: `This is a 'string'`,
+    },
+    {
+      description: "single quotes containing double quotes",
+      input: `'This is a "string"'`,
+      shouldThrow: false,
+      expected: `This is a "string"`,
+    },
+    {
+      description: "single quotes containing escaped single quotes",
+      input: `'This is a \\'string\\''`,
+      shouldThrow: false,
+      expected: `This is a \\'string\\'`,
+    },
+    {
+      description: "double quotes containing escaped double quotes",
+      input: `"This is a \\"string\\""`,
+      shouldThrow: false,
+      expected: `This is a "string"`,
+    },
+    {
+      description: "input not wrapped in quotes",
+      input: `This is a string`,
+      shouldThrow: true,
+    },
+    {
+      description: "empty input",
+      input: ``,
+      shouldThrow: true,
+    },
+  ];
+
+  cases.forEach((c) => {
+    if (c.shouldThrow == true) {
+      assert.throws(
+        () => {
+          resolveParamValue(c.input);
+        },
+        undefined,
+        new Error(
+          `${c.description}: expected resolveParamValue to throw, but it did not`
+        )
+      );
+      return;
+    }
+    if (!c.expected) {
+      return;
+    }
+    const val = resolveParamValue(c.input);
+    assert.equal(
+      c.expected,
+      val,
+      new Error(
+        `${c.description}: expected a resolved parameter value of:\n${c.expected}\n...but got:\n${val}`
+      )
+    );
+  });
+});
+
+Suite("parsePartialParams correctly parses parameter assignments", () => {
+  interface testCase {
+    description: string;
+    shouldThrow: boolean;
+    input: string;
+    expected?: ParameterAssignments;
+  }
+
+  const cases: testCase[] = [
+    {
+      description: "straightforward case",
+      shouldThrow: false,
+      input: `(!includes/example.mdx var1="this is a value" var2='this is also a value'!)`,
+      expected: {
+        var1: `"this is a value"`,
+        var2: `'this is also a value'`,
+      },
+    },
+    {
+      description: "no parameters",
+      shouldThrow: false,
+      input: "(!includes/example.mdx!)",
+      expected: {},
+    },
+    {
+      description: "mismatched quotes within matching quotes",
+      shouldThrow: false,
+      input: `(!includes/example.mdx var1="this is a value' var2='this is also a value"!)`,
+      expected: {
+        var1: `"this is a value' var2='this is also a value"`,
+      },
+    },
+    {
+      description: "mismatched quotes, alternating",
+      input: `(!includes/example.mdx var1="this is a value' var2="this is also a value'!)`,
+      shouldThrow: true,
+    },
+    {
+      description: "escaped quotes",
+      shouldThrow: false,
+      input: `(!includes/example.mdx var1="this is a \\"quote\\"" var2='this is a \\'quote\\''!)`,
+      expected: {
+        var1: `"this is a \\"quote\\""`,
+        var2: `'this is a \\'quote\\''`,
+      },
+    },
+    {
+      description: "end of input before second quote",
+      shouldThrow: true,
+      input: `(!includes/example.mdx var1="this is a value" var2="this is also a value!)`,
+    },
+    {
+      description: "quoted value with no key",
+      shouldThrow: true,
+      input: `(!includes/example.mdx "this is a value" "this is also a value" var1="another value"!)`,
+    },
+    {
+      description: "not an inclusion expression",
+      shouldThrow: true,
+      input: "example.mdx",
+    },
+    {
+      description: "exclamation point",
+      shouldThrow: false,
+      input: `(!error-message.mdx message="Installation has failed!"!)`,
+      expected: {
+        message: `"Installation has failed!"`,
+      },
+    },
+    {
+      description: "escaped quotes",
+      shouldThrow: false,
+      input: `(!error-message.mdx message="Type \\"final\\" to see the final screen."!)`,
+      expected: {
+        message: `"Type \\"final\\" to see the final screen."`,
+      },
+    },
+  ];
+
+  cases.forEach((c) => {
+    if (c.shouldThrow == true) {
+      assert.throws(
+        () => {
+          parsePartialParams(c.input);
+        },
+        undefined,
+        new Error(
+          `${c.description}: expected parsePartialParams to throw, but it did not`
+        )
+      );
+      return;
+    }
+    if (!c.expected) {
+      return;
+    }
+    const val = parsePartialParams(c.input);
+    assert.equal(
+      c.expected,
+      val,
+      new Error(
+        `${c.description}: expected parsed partial params:\n${JSON.stringify(
+          c.expected
+        )}\n...but got:\n${JSON.stringify(val)}`
+      )
+    );
+  });
+});
+
+Suite("Resolves template variables in includes", () => {
+  const value = readFileSync(
+    resolve("server/fixtures/includes-vars.mdx"),
+    "utf-8"
+  );
+
+  const expected = readFileSync(
+    resolve("server/fixtures/includes-vars-result.mdx"),
+    "utf-8"
+  );
+
+  const result = transformer({
+    value,
+    path: "/content/4.0/docs/pages/filename.mdx",
+  }).toString();
 
   assert.equal(result, expected);
 });
