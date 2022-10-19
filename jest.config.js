ab69a14d4fa6c8bac34d46e176392aca540481fa
Paul Gottschling <paul.gottschling@goteleport.com>
2022-10-19 12:09:24 -0400

    Get Jest to load my custom config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,12 @@
 // https://nextjs.org/docs/testing#setting-up-jest-with-the-rust-compiler
 const nextJest = require("next/jest");
 let mdxDocsOptions = {};
-import(".build/server/mdx-config-docs").then((mdxConfig) => {
+
+// Generate an async function that Jest will call when loading its config. This
+// mimics the function signature of the default export of 'next/jest'. See:
+// https://github.com/vercel/next.js/blob/35308c668ee63d8cea5a2c12713d7c761f259764/packages/next/build/jest/jest.ts#L59-L61
+async function createJestConfig() {
+  const mdxConfig = await import("./.build/server/mdx-config-docs.mjs");
   const createJestConfig = nextJest({
     // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
     dir: "./",
@@ -23,35 +28,27 @@ import(".build/server/mdx-config-docs").then((mdxConfig) => {
     },
     transform: {
       "^.+\\.(md|mdx)$": [
-        "jest-transformer-mdx/mdx-options",
+        "jest-transformer-mdx",
         {
           mdxOptions: mdxConfig,
         },
       ],
     },
   };
-
-// Generate an async function that Jest will call when loading its config. This
-// mimics the function signature of the default export of 'next/jest'. See:
-// https://github.com/vercel/next.js/blob/35308c668ee63d8cea5a2c12713d7c761f259764/packages/next/build/jest/jest.ts#L59-L61
-function modifyJestConfig() {
-  return async () => {
-    const loadConf = await createJestConfig(customJestConfig);
-    const loadedConf = await loadConf();
-    loadedConf.transformIgnorePatterns = [
-      // MDX-JS uses ECMAScript modules, so we need to ensure that we can
-      // transform the source. The default next/jest config ignores all
-      // contents of node_modules, so we override that config before passing
-      // it to Jest.
-      //
-      // Context: https://mdxjs.com/docs/troubleshooting-mdx/#esm
-      '/node_modules/(?!@mdx-js)',
-      // Besides '/node_modules/' this is the second default
-      // transformIgnorePatterns value.
-      "^.+\\.module\\.(css|sass|scss)$",
-    ];
-    return loadedConf;
-  };
+  const loadConf = await createJestConfig(customJestConfig);
+  const loadedConf = await loadConf();
+  loadedConf.transformIgnorePatterns = [
+    // MDX-JS uses ECMAScript modules, so we need to ensure that we can
+    // transform the source. The default next/jest config ignores all
+    // contents of node_modules, so we override that config before passing
+    // it to Jest.
+    //
+    // Context: https://mdxjs.com/docs/troubleshooting-mdx/#esm
+    "/node_modules/(?!@mdx-js)",
+    // Besides '/node_modules/' this is the second default
+    // transformIgnorePatterns value.
+    "^.+\\.module\\.(css|sass|scss)$",
+  ];
+  return loadedConf;
 }
-module.exports = modifyJestConfig();
-});
+module.exports = createJestConfig;
