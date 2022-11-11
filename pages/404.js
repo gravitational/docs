130abf8c568615fb23604b4c2a9f57b93627ec86
Steven Martin <steven@goteleport.com>
2022-11-11 15:47:25 -0500

    change links to relative
    Co-authored-by: Reed Loden <reed@reedloden.com>

--- pages/404.js
+++ pages/404.js
@@ -6,7 +6,7 @@ export default function FourOhFour() {
       <h1>404 - Page Not Found</h1>
       We&apos;re very sorry - we could not find the page you were looking for.
       Please navigate to the Teleport{" "}
-      <a href="https://docs.goteleport.com">Documentation</a> or the{" "}
+      <a href="/docs/">Documentation</a> or the{" "}
       <a href="https://goteleport.com">Home Page</a> to find what you&apos;re
       looking for.
     </>
