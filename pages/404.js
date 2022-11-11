c336f2dcbd0c3e34bcd312c33fc56688c7cc06ba
Steven Martin <steven@goteleport.com>
2022-11-11 15:50:14 -0500

    lint fix

--- pages/404.js
+++ pages/404.js
@@ -5,10 +5,8 @@ export default function FourOhFour() {
     <>
       <h1>404 - Page Not Found</h1>
       We&apos;re very sorry - we could not find the page you were looking for.
-      Please navigate to the Teleport{" "}
-      <a href="/docs/">Documentation</a> or the{" "}
-      <a href="/">Home Page</a> to find what you&apos;re
-      looking for.
+      Please navigate to the Teleport <a href="/docs/">Documentation</a> or the{" "}
+      <a href="/">Home Page</a> to find what you&apos;re looking for.
     </>
   );
 }
