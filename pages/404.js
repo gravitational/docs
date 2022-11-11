8b50acee93f32417fc19ab6492f683efed465c7c
Steven Martin <steven@goteleport.com>
2022-11-11 15:34:44 -0500

    fixed lint issues

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,14 @@
 // 404.js
 
 export default function FourOhFour() {
-  return <>
-    <h1>404 - Page Not Found</h1>
-    We're very sorry - we couldn't find the page you were looking for. 
-    Please navigate to the Teleport <a href="https://docs.goteleport.com">Documentation</a> or
-    the <a href="https://goteleport.com">Home Page</a> to find what you're looking for. 
-  </>
+  return (
+    <>
+      <h1>404 - Page Not Found</h1>
+      We&apos;re very sorry - we could not find the page you were looking for.
+      Please navigate to the Teleport{" "}
+      <a href="https://docs.goteleport.com">Documentation</a> or the{" "}
+      <a href="https://goteleport.com">Home Page</a> to find what you&apos;re
+      looking for.
+    </>
+  );
 }
