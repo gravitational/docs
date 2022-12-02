c27e93fd241ebb504ac71fc672d894f837356afd
Paul Gottschling <paul.gottschling@goteleport.com>
2022-12-02 16:04:46 -0500

    Wrap the async MDX compilation in an IIFE
    That way, we can use this in a synchronous process function in the Jest
    transformer.

--- jest-tests/mdx-transformer.mjs
+++ jest-tests/mdx-transformer.mjs
@@ -2,20 +2,34 @@
 // https://github.com/bitttttten/jest-transformer-mdx/issues/25#issuecomment-1041767325
 
 import "path";
-import { compileSync as compileMDXSync } from "@mdx-js/mdx";
+import { compile as compileMDX } from "@mdx-js/mdx";
 import { default as nextJest } from "next/dist/build/swc/jest-transformer.js";
 
 function process(src, filepath, config) {
-  const jsx = compileMDXSync(filepath, {
-    ...config.transformerConfig.mdxOptions,
-  });
-  const transformed = nextJest
-    .createTransformer(config.transformerConfig)
-    .process(`import {mdx} from '@mdx-js/react';${jsx}`, filepath, {
-      config: {},
-    });
+  console.log("running process");
+  let transformed;
+  (async function () {
+    console.log("inside the async function");
+    try {
+      const jsx = await compileMDX(filepath, {
+        ...config.transformerConfig.mdxOptions,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+    console.log("jsx: ", jsx);
+    const transformer = nextJest.createTransformer(config.transformerConfig);
+    transformed = await transformer.processAsync(
+      `import {mdx} from '@mdx-js/react';${jsx}`,
+      filepath,
+      {
+        config: {},
+      }
+    );
 
-  console.log("transformed code:", transformed);
+    console.log("transformed code:", transformed);
+  })();
+  console.log("after the async function");
 
   return transformed;
 }
@@ -24,5 +38,5 @@ function process(src, filepath, config) {
 // called "default" for compatibility with CommonJS. See:
 // https://github.com/facebook/jest/blob/dfc87111e708b9294dc54ab0c17712972d042c1c/packages/jest-util/src/requireOrImportModule.ts#L45
 export default {
-    process: process
-}
+  process: process,
+};
