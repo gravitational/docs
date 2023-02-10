638b53afa2f338e6f121c9e177e41623ec5e2646
Roman Tkachenko <roman@goteleport.com>
2023-02-10 15:13:44 -0800

    Remove GCB checks (#233)

--- .cloudbuild/publish-image.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/env bash
-set -e
-IMAGE_NAME=$DOCKER_REGISTRY/$DOCKER_REPOSITORY:$DOCKER_TAG
-echo Building $IMAGE_NAME
-DOCKER_BUILDKIT=1 docker build -t $IMAGE_NAME .
-echo $QUAYIO_DOCKER_PASSWORD | docker login -u $QUAYIO_DOCKER_USERNAME --password-stdin $DOCKER_REGISTRY
-docker push $IMAGE_NAME
