690f946db04c0299ddb926401b1a763c16cf7323
Paul Gottschling <paul.gottschling@goteleport.com>
2023-03-29 11:32:49 -0400

    Expect the cspell config file in the docs dir
    This avoids over-cluttering the root of gravitational/teleport

--- scripts/check-spelling.sh
+++ scripts/check-spelling.sh
@@ -1,13 +1,13 @@
 # $1 is the content directory in which to check spelling. A content directory is
 # a git submodule pointing to a branch of the gravitational/teleport repository.
 
-npx cspell lint --no-progress --config $1/cspell.json $1/docs/pages/**/*.mdx;
+npx cspell lint --no-progress --config $1/docs/cspell.json $1/docs/pages/**/*.mdx;
 if [ $? -ne 0 ]; then
   cat<<EOF
 
 There are spelling issues in one or more pages within $1/docs/pages (see the
-logs above). Either fix the mispellings or, if these are not actually issues,
-edit the list of ignored words in $1/cspell.json.
+logs above). Either fix the misspellings or, if these are not actually issues,
+edit the list of ignored words in $1/docs/cspell.json.
 
 EOF
 fi
