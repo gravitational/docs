44eb40d2f8ea7ea5ca451ab0560d5097f7d61a42
Paul Gottschling <paul.gottschling@goteleport.com>
2023-03-31 14:27:33 -0400

    Make the spellcheck script more usable in CI
    - Previously, the `yarn spellcheck` script exited with a zero status if
      the `cspell` command exited with an error. This fixes that.
    
    - Require an argument to the `yarn spellcheck` command rather than
      checking the spelling in all content directories by default. This will
      make it easier to run this command in CI, where the content directory
      structure differs a bit from a standard gravitational/docs clone
      (i.e., there's one content directory, not three, and the content
      directory is not named after a version of the docs).

--- scripts/check-spelling.sh
+++ scripts/check-spelling.sh
@@ -1,8 +1,37 @@
 # $1 is the content directory in which to check spelling. A content directory is
 # a git submodule pointing to a branch of the gravitational/teleport repository.
 
+if [ $# -eq 0 ]; then
+
+    cat<<EOF
+
+You must provide an argument to the spellcheck script. The argument must be the
+path to a content directory, which is either:
+
+- A subdirectory of "content" within a gravitational/docs clone
+- The root of a gravitational/teleport clone
+
+EOF
+exit 1;
+fi
+
+if [ ! -e $1/docs/cspell.json ]; then
+
+    cat<<EOF
+
+We could not find $1/docs/cspell.json. Make sure you run the spellcheck script
+on either:
+
+- A subdirectory of "content" within a gravitational/docs clone
+- The root of a gravitational/teleport clone
+
+EOF
+exit 1;
+fi
+
 npx cspell lint --no-progress --config $1/docs/cspell.json $1/docs/pages/**/*.mdx;
-if [ $? -ne 0 ]; then
+RES=$?;
+if [ $RES -ne 0 ]; then
   cat<<EOF
 
 There are spelling issues in one or more pages within $1/docs/pages (see the
@@ -10,5 +39,6 @@ logs above). Either fix the misspellings or, if these are not actually issues,
 edit the list of ignored words in $1/docs/cspell.json.
 
 EOF
+exit $RES;
 fi
 
