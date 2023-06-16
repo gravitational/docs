3105aad96a745804f6fa6c69ad78d84b228174df
Nina Torgunakova <ninatorgunakova@gmail.com>
2023-06-16 20:21:12 +0400

    Return highlighting, support `var` block for Vars

--- server/code-langs.ts
+++ server/code-langs.ts
@@ -1,16 +1,2 @@
 // List of languages in MDX which should support custom plugins such as code snippet remarking
-export const codeLangs = [
-  "code",
-  "yaml",
-  "sh",
-  "shell",
-  "bash",
-  "toml",
-  "json",
-  "ini",
-  "go",
-  "sql",
-  "systemd",
-  "powershell",
-  "protobuf",
-];
+export const codeLangs = ["code", "var"];
