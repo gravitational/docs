bd0655f4b99d63fb14e04951c9e038501cf66587
Paul Gottschling <paul.gottschling@goteleport.com>
2023-06-23 12:14:39 -0400

    Add a transformToAST test
    Do this instead of testing a custom series of processors in
    `remark-includes.test.ts`. Doing this because transformToAST already
    returns an AST, so in theory makes for a cleaner test. Also, I think the
    eventual solution will be to recursively call transformToAST on each
    include, so doing this lets us test this approach directly.
    
    Note that this is currently broken since transformToAST hardcodes a
    number of expectations about the structure of the content directory
    structure.

---
+++ uvu-tests/markdown-config.test.ts
@@ -0,0 +1,64 @@
+import { suite } from "uvu";
+import { resolve } from "path";
+import * as assert from "uvu/assert";
+import { transformToAST } from "../server/markdown-config";
+import { VFile } from "vfile";
+
+// TODO: Remove writeFileSync after finishing testing
+import { readFileSync, writeFileSync } from "fs";
+
+const Suite = suite("server/markdown-config");
+
+Suite.only(
+  "Does not tamper with the expected Markdown AST when including partials",
+  () => {
+    const value = [
+      `<Tabs>
+<TabItem label="option 1">`,
+      "```code",
+      "$ echo 'hello world';",
+      "```",
+      `
+<Admonition type="warning">
+  This is something bad that could happen.
+</Admonition>
+
+</TabItem>
+</Tabs>
+`,
+    ].join("\n");
+
+    let AST1 = transformToAST(
+      value,
+      new VFile({
+        value: value,
+        path: "/content/4.0/docs/pages/filename.mdx",
+      })
+    );
+
+    const value2 = readFileSync(
+      resolve("server/fixtures/include-code-admonition.mdx"),
+      "utf-8"
+    );
+
+    const AST2 = transformToAST(
+      value2,
+      new VFile({
+        value: value2,
+        path: "/content/4.0/docs/pages/filename.mdx",
+      })
+    );
+
+    // TODO: These two calls are for temporary debugging. Remove them after
+    // finishing testing
+    writeFileSync("ast1.json", JSON.stringify(AST1, null, 4));
+    writeFileSync("ast2.json", JSON.stringify(AST2, null, 4));
+
+    console.log("AST1", AST1);
+    console.log("AST2", AST2);
+
+    assert.equal(AST1, AST2);
+  }
+);
+
+Suite.run();
