79c7f59213b6ece3b27054771dfe5bb9b057643c
Paul Gottschling <paul.gottschling@goteleport.com>
2023-09-27 09:42:27 -0400

    Minor version-agnostic page path tweaks (#397)
    * Minor version-agnostic page path tweaks
    
    - Adjust the Upcoming Releases page path
    - Add the contribution guides
    - Add a comment
    
    * Fix version-agnostic page paths
    
    These require an exact match to be deemed version agnostic, so remove
    the `contributing/` item and replace it with each page path within the
    "Contribute to Teleport Documentation" section.

--- utils/useVersionAgnosticPages.ts
+++ utils/useVersionAgnosticPages.ts
@@ -1,6 +1,19 @@
 import { getPathWithoutVersion } from "./url";
 
-const agnost = ["preview/upcoming-releases/", "changelog/"];
+// Paths including these segments are version agnostic. If a URL path includes
+// one of these segments when a user visits a page via a navigation link:
+// - The docs site will render the default version of the page
+// - The version switcher will be disabled on the page
+const agnost = [
+  "upcoming-releases/",
+  "changelog/",
+  "contributing/documentation/",
+  "contributing/documentation/reviewing-docs/",
+  "contributing/documentation/issues/",
+  "contributing/documentation/style-guide/",
+  "contributing/documentation/reference/",
+  "contributing/documentation/how-to-contribute/",
+];
 
 export const useVersionAgnosticPages = () => {
   const isVersionAgnosticPage = (route: string) => {
