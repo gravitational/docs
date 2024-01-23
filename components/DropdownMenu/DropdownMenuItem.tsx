37bc25c162a73c3cc4388e0ee1e38f3fe23c871a
TuukkaIkius <tuukka@ikius.com>
2024-01-23 16:19:05 +0200

    style: convert dropdownmenuitem

--- components/DropdownMenu/DropdownMenuItem.tsx
+++ components/DropdownMenu/DropdownMenuItem.tsx
@@ -1,47 +1,76 @@
 import Link from "components/Link";
 import Image from "next/image";
 import styles from "./DropdownMenuItem.module.css";
-
+import cn from "classnames";
 export interface MenuItemProps {
-  title: string;
-  href?: string;
-  titleLink?: boolean;
-  isImageLink?: boolean;
-  imageSrc?: string;
+  itemType: string | "normal" | "image";
+  icon?: string | null;
+  title?: string | null;
+  description?: string | null;
+  link: string | null;
+  imageItem?: {
+    imageTitle?: string | null;
+    useMetadata: boolean | null;
+    customImage?: {
+      itemImage: string;
+      itemTitle: string;
+      imageCTA?: string;
+      imageDate?: string;
+    } | null;
+  };
   children?: MenuItemProps[];
-  passthrough?: boolean;
 }
 
 const DropdownMenuItem = ({
+  itemType,
   title,
-  href = "/",
-  passthrough = true, // If no value is sent, default to true
-  titleLink = false,
-  isImageLink = false,
-  imageSrc,
-}: MenuItemProps) => {
-  return !isImageLink ? (
-    <Link href={href} passthrough={passthrough} className={styles.wrapper}>
-      <strong className={`${styles.title} ${titleLink && styles.asTitle}`}>
-        {title}
-      </strong>
-    </Link>
-  ) : (
-    <Link href={href} passthrough={passthrough} className={styles.wrapper}>
-      <div className={styles.imageLink}>
-        <Image
-          src={imageSrc}
-          width={280}
-          height={139}
-          style={{ objectFit: "contain" }}
-          alt="featured resource"
-          sizes="25vw"
-        />
-        <strong className={`${styles.title} ${styles.imageTitle}`}>
-          {title}
-        </strong>
+  link = "",
+  icon,
+  description,
+  imageItem,
+  itemAmount,
+  ...props
+}: MenuItemProps & { itemAmount?: number }) => {
+  const { imageTitle, customImage } = imageItem || {};
+  return itemType !== "image" ? (
+    <Link className={styles.link} href={link} passthrough>
+      <Image src={icon || ""} width={20} height={20} alt="" />
+      <div className={styles.item}>
+        <p className={styles.itemTitle}>{title}</p>
+        {description && <p className={styles.description}>{description}</p>}
       </div>
     </Link>
+  ) : (
+    <div
+      className={cn(
+        styles.wrapper,
+        itemAmount > 3 ? styles.moreThan : styles.lessThan
+      )}
+    >
+      {imageTitle && <h3 className={styles.imageTitle}>{imageTitle}</h3>}
+      <Link className={styles.link} href={link}>
+        <div
+          className={cn(styles.imageItem, itemAmount > 3 && styles.asColumn)}
+        >
+          <div className={styles.imageBox}>
+            <Image
+              src={customImage?.itemImage || ""}
+              width={250}
+              height={150}
+              sizes="250px"
+              alt=""
+            />
+          </div>
+          <div className={cn(styles.item, styles.moreMargin)} {...props}>
+            <p className={styles.imageItemTitle}>{customImage?.itemTitle}</p>
+            {customImage?.imageDate && (
+              <p className={styles.dateText}>{customImage?.imageDate}</p>
+            )}
+            <p className={styles.paragraph}>{customImage?.imageCTA}</p>
+          </div>
+        </div>
+      </Link>
+    </div>
   );
 };
 
