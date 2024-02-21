ad9cf5c03eeb159d6c3e7717733847fd3b54b77c
Alexey Ivanov <mail@alexeyivanov.info>
2024-02-22 02:07:17 +0900

    Fix function call error

--- migration/plugins/remark-update-frontmatter.mjs
+++ migration/plugins/remark-update-frontmatter.mjs
@@ -8,7 +8,7 @@ const isYamlNode = (node) => node.type === "yaml";
 
 export default function remarkUpdateFrontmatter({ version } = {}) {
   return (root) => {
-    visit(root, isYamlNode(node), (node) => {
+    visit(root, isYamlNode, (node) => {
       node.value = `${node.value}\nversion: '${version}'`;
     });
   };
