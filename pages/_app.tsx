605c0b633deb26fa969e6e9dc14065607a3bbeab
TuukkaIkius <tuukka@ikius.com>
2024-04-22 13:02:30 +0300

    chore: add linkedin script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -162,6 +162,31 @@ const Analytics = () => {
           {/* End Google Tag Manager (noscript) */}
         </>
       )}
+      {/* LinkedIn Tracking script */}
+      <Script id="linkedin" type="text/javascript">
+        {`_linkedin_partner_id = "1271444";
+window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+</script><script type="text/javascript">
+(function(l) {
+if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+window.lintrk.q=[]}
+var s = document.getElementsByTagName("script")[0];
+var b = document.createElement("script");
+b.type = "text/javascript";b.async = true;
+b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+s.parentNode.insertBefore(b, s);})(window.lintrk);`}
+      </Script>
+      <noscript>
+        {/* eslint-disable-next-line @next/next/no-img-element*/}
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          alt=""
+          src="https://px.ads.linkedin.com/collect/?pid=1271444&fmt=gif"
+        />
+      </noscript>
     </>
   );
 };
