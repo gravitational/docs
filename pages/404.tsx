12404b880d419a8e6d9330a722f1f112dcd4cd51
marie.mcallister <marie.mcallister@goteleport.com>
2024-05-16 18:50:04 -0700

    404 page updates

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,11 +4,24 @@ import Link from "next/link";
 
 export default function FourOhFour() {
   return (
-    <>
-      <h1>404 - Page Not Found</h1>
-      We&apos;re very sorry - we could not find the page you were looking for.
-      Please navigate to the Teleport <Link href="/">Documentation</Link> or the
-      <a href="/">Home Page</a> to find what you&apos;re looking for.
-    </>
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>404 Page Not Found</h1>
+      <p>Sorry, we couldn't find that page</p>
+      <p>We're very sorry - we couldn't find the page you were looking for.</p>
+      <h2>Pages you may find useful</h2>
+      <ul style={{ listStyleType: 'none', padding: 0 }}>
+      <li><a href="/">Home Page</a></li>
+        <li><Link to="/about">About Us</Link></li>
+        <li><Link to="/blog">Blog</Link></li>
+        <li><Link to="/support">Customer Support</Link></li>
+        <li><Link href="Documentation">Installation</Link></i>
+        <li><Link to="installation.mdx">Installation</Link></li>
+        <li><Link to="server-access">Teleport Server Access</Link></li>
+        <li><Link to="kubernetes-access">Teleport Kubernetes Access</Link></li>
+        <li><Link to="database-access">Teleport Database Access</Link></li>
+        <li><Link to="desktop-access">Teleport Desktop Access</Link></li>
+        <li><Link to="application-access">Teleport Application Access</Link></li>
+      </ul>
+    </div>
   );
 }
