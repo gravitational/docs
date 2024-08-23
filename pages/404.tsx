920ab521222cd46946fd730d4dbb19257094409c
Travis Swientek <travis@tswientek.com>
2024-08-23 16:40:53 -0500

    Addition of PostHog 404 handler for additional insights.

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,27 +1,63 @@
 // 404.js
 
 import Link from "next/link";
+import { sendPageNotFoundError } from "utils/posthog";
+import { useEffect } from "react";
+import SiteHeader from "components/Header";
+import Footer from "layouts/DocsPage/Footer";
 
 export default function FourOhFour() {
+  useEffect(() => {
+    void sendPageNotFoundError(); // Report Error to PostHog for tracking
+  }, []);
+
   return (
-    <div style={{ textAlign: 'center', padding: '50px' }}>
-      <h1>404 Page Not Found</h1>
-      <p>Sorry, we couldn&apos;t find that page</p>
-      <p>We couldn&apos;t find the page you were looking for.</p>
-      <h2>Pages you may find useful</h2>
-      <ul style={{ listStyleType: 'none', padding: 0 }}>
-      <li><a href="/">Home Page</a></li>
-        <li><Link href="/about">About Us</Link></li>
-        <li><Link href="/blog">Blog</Link></li>
-        <li><Link href="/Support">Customer Support</Link></li>
-        <li><Link href="/">Documentation</Link></li>
-        <li><Link href="installation">Installation</Link></li>
-        <li><Link href="server-access">Teleport Server Access</Link></li>
-        <li><Link href="kubernetes-access">Teleport Kubernetes Access</Link></li>
-        <li><Link href="database-access">Teleport Database Access</Link></li>
-        <li><Link href="desktop-access">Teleport Desktop Access</Link></li>
-        <li><Link href="application-access">Teleport Application Access</Link></li>
-      </ul>
+    <div>
+      <SiteHeader />
+      <div style={{ textAlign: "center", padding: "50px" }}>
+        <h1>404 Page Not Found</h1>
+        <p>Sorry, we couldn&apos;t find that page</p>
+        <p>
+          Go back to Documentation <Link href="/">home</Link>?
+        </p>
+        <h2>Other pages you may find useful</h2>
+        <ul style={{ listStyleType: "none", padding: 0 }}>
+          <li>
+            <a href="/">Home Page</a>
+          </li>
+          <li>
+            <Link href="/about">About Us</Link>
+          </li>
+          <li>
+            <Link href="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link href="/Support">Customer Support</Link>
+          </li>
+          <li>
+            <Link href="/">Documentation</Link>
+          </li>
+          <li>
+            <Link href="installation">Installation</Link>
+          </li>
+          <li>
+            <Link href="server-access">Teleport Server Access</Link>
+          </li>
+          <li>
+            <Link href="kubernetes-access">Teleport Kubernetes Access</Link>
+          </li>
+          <li>
+            <Link href="database-access">Teleport Database Access</Link>
+          </li>
+          <li>
+            <Link href="desktop-access">Teleport Desktop Access</Link>
+          </li>
+          <li>
+            <Link href="application-access">Teleport Application Access</Link>
+          </li>
+        </ul>
+      </div>
+      <Footer section={true}></Footer>
     </div>
   );
 }
