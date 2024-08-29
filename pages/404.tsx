e2dea4f5dc57131ff4c35d21cdfe7f14c53442cf
Steven Martin <steven@goteleport.com>
2024-08-29 11:55:09 -0400

    updating 404 page (#491)

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,10 +5,11 @@ import Link from "next/link";
 export default function FourOhFour() {
   return (
     <>
+      <title>404 | Teleport</title>
       <h1>404 - Page Not Found</h1>
       We&apos;re very sorry - we could not find the page you were looking for.
       Please navigate to the Teleport <Link href="/">Documentation</Link> or the
-      <a href="/">Home Page</a> to find what you&apos;re looking for.
+      &nbsp;<a href="/">Home Page</a> to find what you&apos;re looking for.
     </>
   );
 }
