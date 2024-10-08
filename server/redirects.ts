a2679f6a58b31e95c9b27d3405b21e771679e032
Paul Gottschling <paul.gottschling@goteleport.com>
2024-10-08 16:47:30 -0400

    Rewrite redirects to menu pages

--- server/redirects.ts
+++ server/redirects.ts
@@ -12,8 +12,25 @@ export const getRedirects = () => {
     return config.redirects || [];
   });
 
-  return result.map((redirect) => ({
-    from: `/docs${redirect.source}`,
-    to: `/docs${redirect.destination}`,
-  }));
+  return result.map((redirect) => {
+    // If a page is an index page for a section, it has the same name as the
+    // containing subdirectory. If a redirect destination is a menu page,
+    // rewrite it to point to the containing URL path so Docusaurus recognizes
+    // it as a valid redirect.
+    const pathSegs = redirect.destination
+      .replaceAll(new RegExp("/$", "g"), "")
+      .split("/");
+    if (
+      pathSegs.length >= 2 &&
+      pathSegs[pathSegs.length - 1] == pathSegs[pathSegs.length - 2]
+    ) {
+      redirect.destination =
+        pathSegs.slice(0, pathSegs.length - 2).join("/") + "/";
+    }
+
+    return {
+      from: `/docs${redirect.source}`,
+      to: `/docs${redirect.destination}`,
+    };
+  });
 };
